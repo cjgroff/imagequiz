e288907e169916e822baac6e7478dc480d72cb0c
Caleb Groff <cjgroff@email.arizona.edu>
2020-09-29 15:59:14 -0700

    Updates

---
+++ precache-manifest.f92843e608e6fa4c833d81131ce78222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c549e5071df27f064de4d0d163a628fd",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "dd55f600b234ce6f5601",
+    "url": "/imagequiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "558cd267cd6ed04d71e8",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd55f600b234ce6f5601",
+    "url": "/imagequiz/static/js/main.5deb8b9a.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imagequiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
